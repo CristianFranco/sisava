28e6868a81b6a94a6d2ceef01c84d29d81b31d98
CristianFranco <cfa_94@hotmail.com>
2016-10-08 15:28:03 -0500

    conexón a base de datos

---
+++ Procesos/connection.php
@@ -0,0 +1,23 @@
+s<?PHP            
+    function connect(){
+        $servername = "107.180.58.59";
+        $username = "adminRCO";
+        $password = "StrUsr94?";
+        $database = "RCO";
+        $conn = new mysqli($servername, $username, $password, $database);
+        
+        if(mysqli_connect_errno()){
+            echo "Error conectando a la base de datos: " . mysqli_connect_error();
+            exit();
+        }
+        else{
+            $conn->query("SET NAMES 'utf8'");
+            return $conn;
+        }
+        
+    }
+
+    function disconnect($connection){
+        $disconnect = mysqli_close($connection);
+    }
+?>
